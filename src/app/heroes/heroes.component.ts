2606ca2ea55f86c15aba79dd8b9581d347982e00
steevened <alvaradosteven315@gmail.com>
2023-06-30 12:06:14 -0500

    create hero details component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,5 +8,9 @@ import { HEROES } from '../mock.heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   heroes = HEROES;
 }
